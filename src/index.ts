81abfb4a1e88deafbb4988bf98c32952c618ee49
Silver Xu <me@silverxu.com>
2020-05-02 12:24:36 +1000

    docs(readme): add readme

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import { addToTrie, initTrie, getFromTrie, searchInTrie } from './tries/trie';
-
-const trie = initTrie<number>();
-
-addToTrie(trie, 'Hello World', 1);
-
-console.log(searchInTrie(trie, 'lr'));
+export * from './tries/trie';
+export * from './tries/types';
